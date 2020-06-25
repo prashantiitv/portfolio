5a4a728986ab41cc947289f969448865e999f729
Prashant <prashantiitv@gmail.com>
2020-06-25 17:20:54 +0530

    Updates

---
+++ precache-manifest.49a55d2238e535918e6f90c498e5eaed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a0d0fbb4458f749a388fc3191cd60a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "391703db9ccf1677908b",
+    "url": "/portfolio/static/css/main.0f9f4d9f.chunk.css"
+  },
+  {
+    "revision": "229634beeafa00c8d8d0",
+    "url": "/portfolio/static/js/2.c0705ec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c0705ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391703db9ccf1677908b",
+    "url": "/portfolio/static/js/main.d2023068.chunk.js"
+  },
+  {
+    "revision": "8c78779c98e297eec2a1",
+    "url": "/portfolio/static/js/runtime-main.23e48518.js"
+  }
+]);
